11207572d37c9ca2d033488c16584fe0edab3766
Hasibur Rahman <hasib.webdev@gmail.com>
2025-04-12 23:42:29 +0600

    feat(ui): moved window position to top-right

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,21 @@
-pub mod commands;
+mod commands;
 
 use commands::greet;
+use tauri::Manager;
+use tauri_plugin_positioner::{Position, WindowExt};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let win = app.get_webview_window("main").unwrap();
+
+            let _ = win.as_ref().window().move_window(Position::TopRight);
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_clipboard::init())
         .run(tauri::generate_context!())
